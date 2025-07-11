e6316cbf3da16e52c3d9418905cb45c27d540f71
Chetan kumar <126199153+chetankumar9903@users.noreply.github.com>
2025-07-11 23:59:24 +0530

    Create CuisineType.java

---
+++ FoodOrderingApp/food/CuisineType.java
@@ -0,0 +1,13 @@
+package com.aurionpro.food;
+
+public enum CuisineType {
+	INDIAN,
+    ITALIAN,
+    CHINESE,
+    MEXICAN,
+    CONTINENTAL,
+    JAPANESE,
+    THAI,
+    AMERICAN,
+    FRENCH
+}
