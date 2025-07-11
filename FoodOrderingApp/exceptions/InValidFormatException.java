8324665041bad97c3f83aaabb31842922e1ef340
Chetan kumar <126199153+chetankumar9903@users.noreply.github.com>
2025-07-11 23:58:15 +0530

    Add files via upload

---
+++ FoodOrderingApp/exceptions/InValidFormatException.java
@@ -0,0 +1,8 @@
+package com.aurionpro.exceptions;
+
+public class InValidFormatException extends RuntimeException{
+	public InValidFormatException(String message) {
+		super(message);
+	}
+
+}
---
+++ FoodOrderingApp/exceptions/MenuNotAvailableException.java
@@ -0,0 +1,8 @@
+package com.aurionpro.exceptions;
+
+public class MenuNotAvailableException extends RuntimeException {
+	 public MenuNotAvailableException(String message) {
+	        super(message);
+	    }
+
+}
