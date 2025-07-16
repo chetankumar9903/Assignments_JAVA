66762fa7618a8cfe03796143be63e481c243aa45
Chetan kumar <126199153+chetankumar9903@users.noreply.github.com>
2025-07-16 09:01:12 +0530

    Create file

--- FoodOrderingApp/payment/UpiPayment.java
+++ FoodOrderingApp/payment/UpiPayment.java
@@ -1,10 +1,34 @@
 package com.aurionpro.payment;
 
+import java.util.Scanner;
+
 import com.aurionpro.order.Order;
 
 public class UpiPayment implements PaymentStrategy {
+//    @Override
+//    public void pay(Order order) {
+//        System.out.println("Payment done via UPI.");
+//    }
+	
+	private static final Scanner scanner = new Scanner(System.in);
+
     @Override
     public void pay(Order order) {
-        System.out.println("Payment done via UPI.");
-    }
+        System.out.print("Enter UPI ID (e.g., user@upi): ");
+        String upiId = scanner.nextLine().trim();
+        while (!upiId.matches("^[\\w.]+@\\w+$")) {
+            System.out.print("Invalid UPI ID. Enter again: ");
+            upiId = scanner.nextLine().trim();
+        }
+
+        System.out.print("Enter 4-digit UPI PIN: ");
+        String pin = scanner.nextLine().trim();
+        while (!pin.matches("\\d{4}")) {
+            System.out.print("Invalid PIN. Enter 4-digit UPI PIN: ");
+            pin = scanner.nextLine().trim();
+        }
+
+        System.out.println("UPI payment successful!");
+	
+}
 }
