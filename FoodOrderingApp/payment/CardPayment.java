9bd7d8a93620084eed10af0facb70454153f0dc8
Chetan kumar <126199153+chetankumar9903@users.noreply.github.com>
2025-07-16 09:01:59 +0530

    create file

--- FoodOrderingApp/payment/CardPayment.java
+++ FoodOrderingApp/payment/CardPayment.java
@@ -1,10 +1,33 @@
 package com.aurionpro.payment;
 
+import java.util.Scanner;
+
 import com.aurionpro.order.Order;
 
 public class CardPayment implements PaymentStrategy {
+//    @Override
+//    public void pay(Order order) {
+//        System.out.println("Card payment processed successfully.");
+//    }
+	
+	private static final Scanner scanner = new Scanner(System.in);
+
     @Override
     public void pay(Order order) {
-        System.out.println("Card payment processed successfully.");
-    }
+        System.out.print("Enter 16-digit Debit Card Number: ");
+        String cardNumber = scanner.nextLine().trim();
+        while (!cardNumber.matches("\\d{16}")) {
+            System.out.print("Invalid Card Number. Enter 16-digit Card Number: ");
+            cardNumber = scanner.nextLine().trim();
+        }
+
+        System.out.print("Enter 4-digit PIN: ");
+        String pin = scanner.nextLine().trim();
+        while (!pin.matches("\\d{4}")) {
+            System.out.print("Invalid PIN. Enter 4-digit PIN: ");
+            pin = scanner.nextLine().trim();
+        }
+
+        System.out.println("Card payment successful!");
+}
 }
