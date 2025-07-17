0ef793addd63ff80fbd47bd2bb26843b59237f6a
Chetan kumar <126199153+chetankumar9903@users.noreply.github.com>
2025-07-17 16:11:21 +0530

    Create CellAlreadyMarkedException.java

---
+++ TicTacToe-UnitTesting/main/exceptions/CellAlreadyMarkedException.java
@@ -0,0 +1,7 @@
+package com.aurionpro.exception;
+
+public class CellAlreadyMarkedException extends Exception {
+    public CellAlreadyMarkedException(String message) {
+        super(message);
+    }
+}
