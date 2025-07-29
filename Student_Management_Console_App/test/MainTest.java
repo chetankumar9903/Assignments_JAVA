516ed8a7341e6b7916a0f69643d2f550d9b4db05
Chetan kumar <126199153+chetankumar9903@users.noreply.github.com>
2025-07-29 19:12:48 +0530

    Create MainTest.java

---
+++ Student_Management_Console_App/test/MainTest.java
@@ -0,0 +1,13 @@
+package com.aurionpro.test;
+
+import com.aurionpro.controller.MainController;
+
+public class MainTest {
+
+	public static void main(String[] args) {
+		
+		MainController.start();
+
+	}
+
+}
